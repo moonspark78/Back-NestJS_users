1204a7d1e69365b1e1df86337bb97476e1e8b8f3
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-11-08 14:49:57 +0100

    📈 starting create UserService  📈

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(3450);
+  app.enableCors(); // Autorise les requêtes venant d'autres domaines, comme votre frontend
 }
 bootstrap();

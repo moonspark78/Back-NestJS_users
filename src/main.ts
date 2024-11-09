dfb18401d3e895feab1d1051b27a51c2d681af15
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-11-09 18:53:39 +0100

    📈 can now receve data from front   📈

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS
+  app.enableCors({
+    origin: 'http://localhost:4673',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
+
   await app.listen(3450);
 }
 bootstrap();
